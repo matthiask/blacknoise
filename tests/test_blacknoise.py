65275509ff9401565a70580d9583eddc77bf02d2
Matthias Kestenholz <mk@feinheit.ch>
2024-03-18 19:30:13 +0100

    Add a mininal unit test

---
+++ tests/test_blacknoise.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+from blacknoise._impl import BlackNoise
+
+
+def test_app():
+    this = Path(__file__).parent
+    blacknoise = BlackNoise(lambda *_a: None)
+    blacknoise.add(this, "/hello/")
+
+    assert "/hello/__init__.py" in blacknoise._files
+    assert "/hello/foo" not in blacknoise._files
