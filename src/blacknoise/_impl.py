78ca6b1db54b9d3c32214bb2326a0c5598f0e270
Matthias Kestenholz <mk@feinheit.ch>
2024-03-18 18:59:28 +0100

    Add cache headers

--- src/blacknoise/_impl.py
+++ src/blacknoise/_impl.py
@@ -1,25 +1,42 @@
 import os
 
+from starlette.exceptions import HTTPException
 from starlette.responses import FileResponse
 
 
+# Ten years is what nginx sets a max age if you use 'expires max;'
+# so we'll follow its lead
+FOREVER = f"max-age={10 * 365 * 24 * 60 * 60}, public, immutable"
+A_LITTE_WHILE = "max-age=60, public"
+
+
 class Blacknoise:
-    def __init__(self, application):
+    def __init__(self, application, *, immutable_file_test=lambda *a: False):
         self._files = {}
+        self._prefixes = ()
         self._application = application
 
-    def add(self, path, root):
-        print(self, path, root)
-        for base, _dirs, files in os.walk(path):
-            self._files |= {
-                os.path.join(root, file): os.path.join(base, file) for file in files
-            }
+        self._immutable_file_test = immutable_file_test
 
-        print(self._files)
+    def add(self, path, prefix):
+        self._prefixes += (prefix,)
+        for base, _dirs, files in os.walk(path):
+            self._files |= {os.path.join(prefix, file): os.path.join(base, file) for file in files}
 
     async def __call__(self, scope, receive, send):
-        if file := self._files.get(scope["path"]):
-            response = FileResponse(file)
-            await response(scope, receive, send)
-        else:
+        path = os.path.normpath(scope["path"].removeprefix(scope["root_path"]))
+        if not path.startswith(self._prefixes):
             await self._application(scope, receive, send)
+            return
+
+        assert scope["type"] == "http"
+        if scope["method"] not in ("GET", "HEAD"):
+            raise HTTPException(status_code=405)
+
+        if file := self._files.get(path):
+            headers = {"cache-control": FOREVER if self._immutable_file_test(path) else A_LITTE_WHILE}
+            response = FileResponse(file, headers=headers)
+            await response(scope, receive, send)
+            return
+
+        raise HTTPException(status_code=404)
