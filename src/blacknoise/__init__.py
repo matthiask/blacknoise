cce186117c07482b6d77304e4a819ce388ae8a78
Matthias Kestenholz <mk@feinheit.ch>
2024-03-18 19:10:38 +0100

    Allow all origins, add cache headers

--- src/blacknoise/__init__.py
+++ src/blacknoise/__init__.py
@@ -1,3 +1,8 @@
 # SPDX-FileCopyrightText: 2024-present Matthias Kestenholz <mk@feinheit.ch>
 #
 # SPDX-License-Identifier: MIT
+
+try:
+    from blacknoise._impl import Blacknoise  # noqa
+except ModuleNotFoundError:
+    pass
--- src/blacknoise/_impl.py
+++ src/blacknoise/_impl.py
@@ -3,7 +3,6 @@ import os
 from starlette.exceptions import HTTPException
 from starlette.responses import FileResponse
 
-
 # Ten years is what nginx sets a max age if you use 'expires max;'
 # so we'll follow its lead
 FOREVER = f"max-age={10 * 365 * 24 * 60 * 60}, public, immutable"
@@ -11,7 +10,7 @@ A_LITTE_WHILE = "max-age=60, public"
 
 
 class Blacknoise:
-    def __init__(self, application, *, immutable_file_test=lambda *a: False):
+    def __init__(self, application, *, immutable_file_test=lambda *_a: False):
         self._files = {}
         self._prefixes = ()
         self._application = application
@@ -21,7 +20,9 @@ class Blacknoise:
     def add(self, path, prefix):
         self._prefixes += (prefix,)
         for base, _dirs, files in os.walk(path):
-            self._files |= {os.path.join(prefix, file): os.path.join(base, file) for file in files}
+            self._files |= {
+                os.path.join(prefix, file): os.path.join(base, file) for file in files
+            }
 
     async def __call__(self, scope, receive, send):
         path = os.path.normpath(scope["path"].removeprefix(scope["root_path"]))
@@ -29,12 +30,16 @@ class Blacknoise:
             await self._application(scope, receive, send)
             return
 
-        assert scope["type"] == "http"
-        if scope["method"] not in ("GET", "HEAD"):
+        if scope["type"] != "http" or scope["method"] not in ("GET", "HEAD"):
             raise HTTPException(status_code=405)
 
         if file := self._files.get(path):
-            headers = {"cache-control": FOREVER if self._immutable_file_test(path) else A_LITTE_WHILE}
+            headers = {
+                "access-control-allow-origin": "*",
+                "cache-control": (
+                    FOREVER if self._immutable_file_test(path) else A_LITTE_WHILE
+                ),
+            }
             response = FileResponse(file, headers=headers)
             await response(scope, receive, send)
             return
