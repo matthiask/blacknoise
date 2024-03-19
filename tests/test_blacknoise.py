a31d2ada466087785447eaa5fc2f4f291e37cc70
Matthias Kestenholz <mk@feinheit.ch>
2024-03-19 18:24:54 +0100

    Verify the content-encoding header value

--- tests/test_blacknoise.py
+++ tests/test_blacknoise.py
@@ -41,14 +41,17 @@ async def test_static_file_serving(bn):
         r = await client.get("/static/hello.txt")
         assert r.status_code == 200
         assert r.text == "world\n"
+        assert r.headers["content-encoding"] == "gzip"
 
         r = await client.get("/static/hello2.txt.gz")
         assert r.status_code == 200
         assert r.text == "world2\n"
+        assert r.headers["content-encoding"] == "gzip"
 
         r = await client.get("/static/hello3.txt")
         assert r.status_code == 200
         assert r.text == "world3\n"
+        assert "content-encoding" not in r.headers
 
         r = await client.post("/static/hello.txt")
         assert r.status_code == 405
